732252efad35fc75ed76bc1177072d4d6042d6f6
alidep <amdepsky@gmail.com>
2018-06-26 02:21:43 -0300

    Add files via upload

---
+++ data2.js
@@ -0,0 +1,143 @@
+var data1 = {
+    "nodes": [
+	{
+	    "name": "Gustavo-Petro",
+	    "photo": "2.png",
+	    "answer": "Si",
+	    "fullanswer": "Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.",
+	    "similarity": 20
+	},
+	{
+	    "name": "Sergio-Fajardo",
+	    "photo": "3.png",
+	    "answer": "No",
+	    "fullanswer": "Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.",
+	    "similarity": 3
+	},
+	{
+	    "name": "Humberto-De-La-Calle",
+	    "photo": "5.png",
+	    "answer": "No, pero",
+	    "similarity": 40
+	},
+	{
+	    "name": "Jorge-Antonio-Trujillo-Sarmiento",
+	    "photo": "6.png",
+	    "answer": "Si",
+	    "similarity": 15
+	},
+	{
+	    "name": "Iván-Duque",
+	    "photo": "1.png",
+	    "answer": "Si, pero",
+	    "similarity": 70
+	},
+	{
+	    "name": "Iván-Duque22",
+	    "photo": "1.png",
+	    "answer": "Sin respuesta",
+	    "similarity": 70
+	},
+	{
+	    "name": "Germán-Vargas-Lleras",
+	    "photo": "4.png",
+	    "answer": "No, pero",
+	    "similarity": 30
+	}
+    ]
+}
+
+var data3 = {
+    "nodes": [
+	{
+	    "name": "Gustavo-Petro",
+	    "photo": "2.png",
+	    "answer": "No",
+	    "similarity": 20
+	},
+	{
+	    "name": "Sergio-Fajardo",
+	    "photo": "3.png",
+	    "answer": "Si",
+	    "similarity": 3
+	},
+	{
+	    "name": "Humberto-De-La-Calle",
+	    "photo": "5.png",
+	    "answer": "No, pero",
+	    "similarity": 40
+	},
+	{
+	    "name": "Jorge-Antonio-Trujillo-Sarmiento",
+	    "photo": "6.png",
+	    "answer": "Si",
+	    "similarity": 15
+	},
+	{
+	    "name": "Iván-Duque",
+	    "photo": "1.png",
+	    "answer": "Si, pero",
+	    "similarity": 70
+	},
+	{
+	    "name": "Iván-Duque22",
+	    "photo": "1.png",
+	    "answer": "No",
+	    "similarity": 70
+	},
+	{
+	    "name": "Germán-Vargas-Lleras",
+	    "photo": "4.png",
+	    "answer": "No, pero",
+	    "similarity": 30
+	}
+    ]
+}
+
+var data2 = {
+    "nodes": [
+	{
+	    "name": "Gustavo-Petro",
+	    "photo": "2.png",
+	    "answer": "Si",
+	    "similarity": 20
+	},
+	{
+	    "name": "Sergio-Fajardo",
+	    "photo": "3.png",
+	    "answer": "Si",
+	    "similarity": 3
+	},
+	{
+	    "name": "Humberto-De-La-Calle",
+	    "photo": "5.png",
+	    "answer": "No, pero",
+	    "similarity": 40
+	},
+	{
+	    "name": "Jorge-Antonio-Trujillo-Sarmiento",
+	    "photo": "6.png",
+	    "answer": "Si",
+	    "similarity": 15
+	},
+	{
+	    "name": "Iván-Duque",
+	    "photo": "1.png",
+	    "answer": "Si, pero",
+	    "similarity": 70
+	},
+	{
+	    "name": "Iván-Duque22",
+	    "photo": "1.png",
+	    "answer": "No",
+	    "similarity": 70
+	},
+	{
+	    "name": "Germán-Vargas-Lleras",
+	    "photo": "4.png",
+	    "answer": "No, pero",
+	    "similarity": 30
+	}
+    ]
+}
+
